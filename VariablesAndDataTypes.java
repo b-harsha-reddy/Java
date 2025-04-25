c8ea41372da89b0021808db75f1ea9434e9653c0
b-harsha-reddy <harshabhvr1@gmail.com>
2025-04-25 11:29:42 +0530

    VariablesAndDataTypes

---
+++ VariablesAndDataTypes.java
@@ -0,0 +1,20 @@
+public class VariablesAndDataTypes{
+    public static void main(String[] args){
+        //This is a single line comment
+
+        /*
+        This is a multi-line comment
+        */
+
+       //Variable declaration
+       int num_1 = 10;
+       int num_2 = 20;
+       int sum = num_1 +num_2;
+       //Printing the sum
+       System.out.println("Sum:"+sum);
+
+
+    }
+
+}
+
