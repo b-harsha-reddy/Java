d66336140842bbeb66a4b984702804cbba73830d
b-harsha-reddy <harshabhvr1@gmail.com>
2025-04-25 20:41:54 +0530

    pyramid

---
+++ Pyramid1.java
@@ -0,0 +1,15 @@
+//This program prints Number Pyramid
+public class Pyramid1{
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        for(int i = 1; i<=num ;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+       
+    }
+}
